6851c6e7a465a5e111da96855ef3419181a320e8
Lea Model <lea.skywalker309@gmail.com>
2023-06-26 14:57:30 +0200

    Maze-tiles creation for easy and hard mode added (different maze- sizes)

--- src/game.js
+++ src/game.js
@@ -6,6 +6,8 @@ class Maze {
         this.gameScreen = document.getElementById("game-screen");
         this.gameEndScreen = document.getElementById("end-screen");
         this.player = null;
+        this.gameModeEasy = true;
+        this.sizeOfMaze = 30;
         this.mazeSolution = [] //hexagon ids - randomly chosen
         this.timeCount = 60;
         this.lives = 3;
@@ -14,8 +16,33 @@ class Maze {
 
     start() {
         // change screens
+        /* to be added later: different size game containers for difficulty levels */
         this.startScreen.style.display = "none";
         this.gameScreen.style.display = "flex";
+
+        if (!this.gameModeEasy) {
+            this.sizeOfMaze = 60;
+        }
+
+        this.createMaze();
+    }
+
+    createMaze() {
+        // create tiles
+        for(let i = 1; i <= this.sizeOfMaze; i++){
+            let addDiv = document.createElement('div');
+            addDiv.setAttribute("id", i);
+
+            document.getElementById("game-container").appendChild(addDiv);
+        }
+
+
+        // create array for possible tile-connections to randomly create a way through the maze
+        let mazeCalcArr = [];
+         
+        // 1. calc amount of tiles per row
+        // const tilesPerRow = document.getElementByClass("container").style.width
+        // console.log(tilesPerRow);
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 //  script to start & end the game 
 window.addEventListener('load', () => {
     const startBtnEasy = document.getElementById("start-easy")
-    // const startBtnHard = document.getElementById("start-hard")
+    const startBtnHard = document.getElementById("start-hard")
     // const restartBtn = document.getElementById("restart")
 
-    let game;
+    let game = new Maze();
 
     function startGame (){
         console.log("game starts");
-        game = new Maze()
         game.start()
     }
 
     startBtnEasy.addEventListener('click', () => {
+        game.gameModeEasy = true;
+        startGame()
+    })
+
+    startBtnHard.addEventListener('click', () => {
+        game.gameModeEasy = false;
         startGame()
     })
 
