ac2d4331f4f57a851dc5a21d4006ad96542f9daf
Lea Model <lea.skywalker309@gmail.com>
2023-06-26 20:45:43 +0200

    Tiles will be identified by their ID based on the maze array. Issues with calculation and game screen size need to be fixed

--- src/game.js
+++ src/game.js
@@ -29,45 +29,75 @@ class Maze {
 
     createMaze() {
         // create tiles-matrix
-        for(let i = 1; i <= this.sizeOfMaze; i++){
+        for (let i = 1; i <= this.sizeOfMaze; i++) {
             let addDiv = document.createElement('div');
             addDiv.setAttribute("id", i);
-
+            addDiv.innerHTML = `${i}`
+        
             document.getElementById("game-container").appendChild(addDiv);
         }
 
 
-         
+
         // 1. calc amount of tiles per row   !!! limit to equal-length rows
         const containerWidth = document.getElementById("game-container").offsetWidth
         const hexagonWidth = document.getElementById("1").offsetWidth + 8
 
         // create array for possible tile-connections to randomly create a way through the maze
         const hexagonsPerRow = Math.floor(containerWidth / hexagonWidth)
-        let mazeCalcArr = [-(hexagonsPerRow - 1), 1, (hexagonsPerRow - 1), hexagonsPerRow];
+        let mazeCalcArr = [-(hexagonsPerRow), 1, (hexagonsPerRow)];
 
         // define start-ids - ?? set grid limitation - equal amount of tiles per row
         let startTilesId = []
-        for (let i = 1; i <= this.sizeOfMaze; i + hexagonsPerRow){
-                startTilesId.push(i)
+
+        for (let i = 1; i <= this.sizeOfMaze; i += hexagonsPerRow) {
+            startTilesId.push(i)
         }
 
         // define end-ids
         let endTilesId = []
-        for (let i = hexagonsPerRow; i < this.sizeOfMaze; i + hexagonsPerRow){
-            startTilesId.push(i)
+        for (let i = hexagonsPerRow; i < this.sizeOfMaze; i += hexagonsPerRow) {
+            endTilesId.push(i)
         }
 
         // create random way through the maze, set the starting point
-        let wayThroughMaze = [startTilesId[Math.floor(Math.random() * startTilesId.length)]]
+        let wayThroughMaze = []
+        wayThroughMaze.push(startTilesId[Math.floor(Math.random() * startTilesId.length)]) //works
         
-        // create random way based on options. Stop, when outer tile has been reached
-        let randomStepId = Math.floor(Math.random() * 4);
+        //not uodating: let wayArrLength = wayThroughMaze.length - 1
 
-        do {
-            wayThroughMaze.push(mazeCalcArr[randomStepId]);
-        } while (!endTilesId.includes(randomStepId))
 
+        let randomArrId;
+        // repeatedly get random index-nr between 0 - 3 in this case
+        for (let i = 0; i < this.sizeOfMaze; i++) {
+
+            // create random index to chose the next move
+            randomArrId = [Math.floor(Math.random() * mazeCalcArr.length)]
+            // add or subtract the next move from the last position
+            if(wayThroughMaze[wayThroughMaze.length - 1]%2 === 0 && randomArrId === 0 ){
+                randomArrId = wayThroughMaze[wayThroughMaze.length - 1] + mazeCalcArr[randomArrId] + 1
+            } else {
+                randomArrId = wayThroughMaze[wayThroughMaze.length - 1] + mazeCalcArr[randomArrId]
+            }
+
+            if (!wayThroughMaze.includes(randomArrId) && 0 < randomArrId && randomArrId < this.sizeOfMaze) {
+                if (endTilesId.includes(randomArrId)) { // check doesnt seem to work always
+                    wayThroughMaze.push(randomArrId);
+                    break;
+                } else {
+                    wayThroughMaze.push(randomArrId);
+                }
+            } else {``
+                continue;
+            }
+            console.log(wayThroughMaze);
+        }
+
+        let solution;
+        for(let i = 0; i < wayThroughMaze.length; i++){
+            solution = document.querySelector(`[id="${wayThroughMaze[i]}"]`)
+            solution.style.background = "rgb(231, 19, 164)";
+        }
     }
 }
 
